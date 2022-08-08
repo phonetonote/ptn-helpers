94077b0cb91dc9abc48f1919bbd86450b04820a8
Scott Block <scottrblock@gmail.com>
2022-08-08 18:36:35 -0400

    Simplify types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { format } from "date-fns";
-import { FeedItem, PtnNode, FeedLinkKey } from "./types";
 
 export const FEED_LINK_KEYS = [
   "title",
@@ -7,6 +6,28 @@ export const FEED_LINK_KEYS = [
   "_ptr_open_graph_site_name",
   "_ptr_open_graph_type",
 ];
+
+export type FeedLinkKey = typeof FEED_LINK_KEYS[number];
+export type FeedAttachment = {
+  [feedLinkKey in FeedLinkKey]?: string | undefined;
+} & {
+  _ptr_media_type: string;
+  _ptr_open_graph_image_url?: string;
+  url?: string;
+};
+export type FeedItem = {
+  id: string;
+  date_published: string;
+  url: string;
+  content_text: string;
+  attachments?: FeedAttachment[];
+  _ptr_sender_type: string;
+};
+export type PtnNode = {
+  text: string;
+  children: PtnNode[];
+  uid?: string;
+};
 
 export const itemToNode = (feedItem: FeedItem, hashtag: string): PtnNode => {
   const children: PtnNode[] = [];
